aa2680bc2378a7e0acb9745c0f652328038d7570
Ege <egeikibin@gmail.com>
2022-03-13 18:25:22 +0300

    fixed the algorithm and bugs, added gameplay loop, will add scoring in the future

--- script.js
+++ script.js
@@ -1,73 +1,97 @@
-// computer plays 
-let compPlay;
-let index;
-const rps = ["rock", "paper", "scissors"]
 
-const computerPlay = () => {
-    let randomNum = Math.floor(Math.random() * 3);
-    console.log("Random Number: " + randomNum);
-    compPlay = rps[randomNum];
-    console.log("Computer Hand: " + compPlay); 
-    console.log("index of computer hand in computerPlay " + rps.indexOf(compPlay));
-    return compPlay;
-}
+const Program = () => {
+    const rps = ["rock", "paper", "scissors"]
 
-computerPlay();
 
-const indexCalc = (compPlay) => {
-    let index = rps.indexOf(compPlay);
-    return index; 
-}
-
-indexCalc(compPlay);
-
-console.log("index of computer hand in global: " + index);
-
-const playerSelection = () => {
-    const playerHand = prompt("Please play your hand; rock, paper or scissors?").toLowerCase();
-    console.log("Player Hand: " + playerHand);
-    const err = "Please play a valid hand.";
-    for (var i = 0; i < rps.length; i++) {
-        if (playerHand == rps[i]) {
-            console.log("Player hand is valid");
+    const computerPlay = () => {
+        let randomNum = Math.floor(Math.random() * 3);
+        console.log("Random Number: " + randomNum);
+        compPlay = rps[randomNum];
+        console.log("Computer Hand: " + compPlay); 
+        return compPlay;
+    }
+    
+    computerPlay();
+    
+    const indexOfComp = rps.indexOf(compPlay);
+    
+    const playerSelection = () => {
+        playerHand = prompt("Please play your hand; rock, paper or scissors?").toLowerCase();
+        return playerHand;
+    }
+    playerSelection();
+    
+    const checkIfValid = (playerHand) => {
+        const err = "Please play a valid hand.";
+        if (rps.includes(playerHand)) {
             return playerHand;
         }
-        // else {
-        //     alert(err); 
-        //     playerSelection(); 
-        // }
+        else {
+            alert(err); 
+            playerSelection(); 
+        }
     }
-}
+    
+    checkIfValid(playerHand);
 
-playerSelection();
-
-const checkWinLose = (index, playerHand) => {
-
-    console.log("index of computer hand in checkWinLose: " + index);
-    if (index === rps.indexOf(playerHand)) {
-        alert("It's a tie!");
-    } else if (rps.indexOf(playerHand) == rps.indexOf(index + 1)) { //player wins
-        playerWins = true; 
+    const playAgain = () => {
+        let answer= prompt("Play again? y/n").toLowerCase();
+        console.log(answer);
+        if (answer == "y") {
+            Program();
+            return
+        } else if (answer == "n"){
+            alert("Goodbye.");
+            return
+        } else {
+            alert("Please enter a valid answer.");
+            playAgain();
+            return
+        }
+    }
+    
+    let endDisplay = "You chose " + playerHand + ", the computer chose " + compPlay + " .";
+    let playerWins;
+    
+    const checkWinLose = (indexOfComp, playerHand) => {
+        switch (rps.indexOf(playerHand)-indexOfComp) {
+            case 0: //tie
+                alert(endDisplay + " It's a tie!");
+                playAgain();
+                break;
+            case 1: //player win
+                playerWins = true;
+                break;
+            case -2: //player win
+                playerWins = true;
+                break;
+            default: //player lose
+                playerWins = false;
+                break;
+        }
         return playerWins;
-    } else {
-        playerWins = false;
-        console.log("else ran.");
-        return playerWins;
     }
+    
+    checkWinLose(indexOfComp, playerHand);
+    
+    const displayWinLose = () => {
+        if (playerWins == true) {
+            const won = " You win!";
+            alert(endDisplay + " " + won);
+            return
+        } else {
+            const lost = " You lost :(";
+            alert(endDisplay + " " + lost);
+            return
+        }
+    }
+    
+    displayWinLose(playerWins);
+
+   
+    
+    playAgain();
 }
 
-checkWinLose();
+Program();
 
-const displayWinLose = (playerWins) => {
-    let endDisplay = "You have played: " + playerHand + ", the computer have played: " + compPlay;
-    if (playerWins == true) {
-        const won = "You win!";
-        alert(endDisplay + " " + won);
-    }
-    else if (playerWins == false) {
-        const lost = "You lost :(";
-        alert(endDisplay + " " + lost);
-    }
-}
-
-displayWinLose();
